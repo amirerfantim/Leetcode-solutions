f81ca252406ce16dbae481fdaad4f8b85b83d254
amirerfantim <amirerfantim@gmail.com>
2022-12-20 13:10:30 +0330

    adding 278

---
+++ 278.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+def isBadVersion(mid):
+    pass
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        r = n
+        while l < r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid 
+            else:
+                l = mid + 1
+        return l
