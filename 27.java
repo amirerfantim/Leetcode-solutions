1fc49f0a93613b614f355913ec452911af5e77b7
amirerfantim <amirerfantim@gmail.com>
2023-07-06 22:25:56 +0330

    adding 27

---
+++ 27.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int loop = 0; loop < nums.length; loop++){
+            if (nums[loop] == val){
+                k += 1;
+            }
+            if (nums[loop] != val){
+                nums[loop - k] = nums[loop];
+            }
+        }
+        return nums.length - k;
+    }
+}
