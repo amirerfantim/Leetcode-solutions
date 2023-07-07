e1f1ffc39c3dcaded380bb4fca18a24e2be4c390
amirerfantim <amirerfantim@gmail.com>
2023-07-07 15:38:09 +0330

    adding 26

---
+++ 26.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int org_index = 0;
+        int unique_index = 1;
+
+        for (org_index = 0 ; org_index < nums.length - 1; org_index++){
+            if (nums[org_index] != nums[org_index + 1]){
+                nums[unique_index] = nums[org_index + 1];
+                unique_index += 1;
+
+
+            }
+
+        }
+        return unique_index;
+    }
+}
