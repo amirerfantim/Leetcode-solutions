0f808aad174ea099de6065b5b161ea898cfe3f38
amirerfantim <amirerfantim@gmail.com>
2023-10-22 12:30:54 +0330

    adding 392

---
+++ 392.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        cur_index = -1
+        for char_s in s:
+            print(t[cur_index+1:])
+            if char_s in t[cur_index + 1:]:
+                new_index = t[cur_index+1:].index(char_s)
+                cur_index = cur_index + new_index + 1
+                # new = t[:cur_index] + t[cur_index+1:]
+                # t = new
+            else:
+                return False
+        return True
+
+
+s1 = Solution()
+s = "aaa"
+t = "bbaaaa"
+print(s1.isSubsequence(s, t))
