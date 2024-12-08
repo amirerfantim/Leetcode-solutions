469f77da68efe74551ddb2f9c8a6175c91ea290d
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-12-08 19:41:59 +0330

    Adding 122 with python

---
+++ 122.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profit += prices[i + 1] - prices[i]
+        print(profit)
+        return profit
