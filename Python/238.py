a73457d2a5e396977bcf767137aad749cb17a891
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-12-13 14:26:42 +0330

    Adding 238 using python

--- Python/238.py
+++ Python/238.py
@@ -3,18 +3,16 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        all_products = 1
-        products = []
-        for num in nums:
-            all_products *= num
+        result = [1] * len(nums)
+        for index in range(1, len(nums)):
+            result[index] = result[index - 1] * nums[index - 1]
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                product = 1
-                for j in range(len(nums)):
-                    if j != i:
-                        product *= nums[j]
-                products.append(product)
-            else:
-                products.append(all_products // nums[i])
-        return products
+        multiplier = 1
+        for reversed_index in range(len(nums) - 2, -1, -1):
+            multiplier *= nums[reversed_index + 1]
+            result[reversed_index] = result[reversed_index] * multiplier
+
+
+s1 = Solution()
+s1.productExceptSelf(nums=[-1,1,0,-3,3])
+
