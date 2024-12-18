5481fa698d2baf94f3ecc6633d029d5beafceb77
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-12-18 15:03:10 +0330

    Adding 151 using python

--- Python/151.py
+++ Python/151.py
@@ -1,16 +1,9 @@
 class Solution(object):
     def reverseWords(self, s):
-        s = s.strip()
-        words = s.split()
-        r_words = words[::-1]
-        res = ""
+        return ' '.join(reversed(s.split()))
 
-        for i in range(len(r_words)):
-            res += r_words[i]
-            if i != len(r_words) - 1:
-                res += " "
-        return res
+
 
 s1 = Solution()
-s = "the sky is blue"
-s1.reverseWords(s)
+s = "the sky    is blue   "
+print(s1.reverseWords(s))
