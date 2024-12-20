65341453c934a26b838fbb50185a209423e2b708
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-12-21 02:37:22 +0330

    Adding 167 using python

--- Python/167.py
+++ Python/167.py
@@ -3,22 +3,20 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left = 0
+        left  = 0
         right = len(numbers) - 1
-        res = []
         while left < right:
-            cur_sum = numbers[left] + numbers[right]
-            if cur_sum > target:
-                right -= 1
-            elif cur_sum == target:
-                res = [left + 1, right + 1]
-                break
-            else:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
                 left += 1
-        return res
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+
+        return []
 
 
 s1 = Solution()
-numbers = [2, 7, 11, 15]
-target = 9
+numbers = [2,3,4]
+target = 6
 print(s1.twoSum(numbers, target))
