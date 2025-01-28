877f24f56617cf78adbc6048af04c6fabf657fca
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-01-29 00:27:25 +0330

    Adding 217 using python

--- Python/217.py
+++ Python/217.py
@@ -1,15 +1,17 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for x, y in zip(nums[:-1], nums[1:]):
-            if x == y:
+        counter_dict = defaultdict(int)
+        for num in nums:
+            counter_dict[num] += 1
+            if counter_dict[num] > 1:
                 return True
         return False
 
 
 s1 = Solution()
-nums = [1, 2, 3, 4]
+nums = [1, 1, 3, 4]
 print(s1.containsDuplicate(nums))
