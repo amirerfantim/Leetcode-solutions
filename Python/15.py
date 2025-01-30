c76f69aade4b4bad8c0529e7d4f8806b1133e4f5
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-01-30 16:48:09 +0330

    Adding 15 using python

--- Python/15.py
+++ Python/15.py
@@ -5,22 +5,24 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result = []
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i - 1]:
                 continue
-            current_target = -nums[i]
-            left = i + 1
-            right = len(nums) - 1
+
+            left, right = i + 1, len(nums) - 1
             while left < right:
-                if nums[left] + nums[right] == current_target:
-                    result.append([nums[i], nums[left], nums[right]])
+                total = num + nums[left] + nums[right]
+                if total == 0:
+                    result.append([num, nums[left], nums[right]])
                     left += 1
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
-                elif nums[left] + nums[right] < current_target:
+                elif total < 0:
                     left += 1
-                elif nums[left] + nums[right] > current_target:
+                else:
                     right -= 1
+
         return result
 
 
