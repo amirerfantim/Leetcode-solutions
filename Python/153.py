6eecf1cfb45d78026dc7b3f82e286b75ccb88e78
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-01 13:52:50 +0330

    Adding 153 using python

---
+++ Python/153.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
+
+
+s1 = Solution()
+print(s1.findMin([4, 5, 6, 0, 1, 2, 3]))
