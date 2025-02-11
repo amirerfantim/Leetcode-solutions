ba7007f0f85ba319fa3e719f93ec1d5cc7e5debf
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-11 19:58:21 +0330

    Adding 215 using python

--- Python/215.py
+++ Python/215.py
@@ -1,7 +1,17 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        sorted_nums = nums.sort(reverse=True)
-        return nums[k-1]
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for num in nums[k:]:
+            if num > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+        return heap[0]
+
+
+s1 = Solution()
+print(s1.findKthLargest([3, 2, 1, 5, 6, 4], 1))
