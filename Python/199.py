f3aac8cdb9630c3e0db9e33c8f873db9dd5be976
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-11 21:44:15 +0330

    Adding 199 using python

--- Python/199.py
+++ Python/199.py
@@ -16,17 +16,16 @@ class Solution:
             return []
         queue = deque()
         queue.append(root)
-        res = [root.val]
-        while len(queue) > 0:
-            i = 0
-            length = len(queue)
-            while i < length:
-                i += 1
-                cur = queue.popleft()
-                if cur.right:
-                    queue.append(cur.right)
-                if cur.left:
-                    queue.append(cur.left)
-            if len(queue) > 0:
-                res.append(queue[0].val)
-        return res
+        result = []
+
+        while queue:
+            result.append(queue[-1].val)
+            child_queue = deque()
+            while queue:
+                current = queue.popleft()
+                if current.left:
+                    child_queue.append(current.left)
+                if current.right:
+                    child_queue.append(current.right)
+            queue = child_queue
+        return result
