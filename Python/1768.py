90878430707f2efa9056856850caae24fdb93e61
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-12 20:09:16 +0330

    Adding 1768 using python

--- Python/1768.py
+++ Python/1768.py
@@ -1,16 +1,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res_word = []
-        for i in range(min(len(word1), len(word2))):
-            res_word.append(word1[i])
-            res_word.append(word2[i])
-        res_word.append(word1[i+1:])
-        res_word.append(word2[i+1:])
-        return "".join(res_word)
+        res = ""
+        len1 = len(word1)
+        len2 = len(word2)
+        for i in range(max(len1, len2)):
+            if i < len1:
+                res += word1[i]
+            if i < len2:
+                res += word2[i]
+
+        return res
+
 
 s1 = Solution()
 word1 = "abc"
 word2 = "pqr"
 print(s1.mergeAlternately(word1, word2))
-
-
