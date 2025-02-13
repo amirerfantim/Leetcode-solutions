d4b92ddf65ba95e9c0579abf44d28a473323010e
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-14 02:12:56 +0330

    Adding 328 using python

---
+++ Python/328.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        odd = head
+        temp = head.next
+        even = head.next
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+
+            odd = odd.next
+            even = even.next
+        odd.next = temp
+        return head
