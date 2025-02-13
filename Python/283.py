01def2e4e0e6820492aa1a356daec88fd62943a0
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-13 14:11:28 +0330

    Adding 283 using python

--- Python/283.py
+++ Python/283.py
@@ -1,17 +1,15 @@
 from typing import List
 
 
-class Solution(object):
-    def moveZeroes(self, nums):
-
-        zero = []
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zero.append(i)
-        for j in range(len(zero)):
-            nums.append(nums.pop(zero[j] - j))
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero_pointer = 0
+        for main_pointer in range(len(nums)):
+            if nums[main_pointer] != 0:
+                nums[main_pointer], nums[zero_pointer] = nums[zero_pointer], nums[main_pointer]
+                zero_pointer += 1
 
 
 s1 = Solution()
 nums = [0, 1, 0, 3, 12]
-print(s1.moveZeroes(nums))
+s1.moveZeroes(nums)
