c2731903dec484aa50b2ad2961bf9239400f0dde
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-14 03:14:31 +0330

    Adding 162 using python

---
+++ Python/162.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+
+        return l
+
+
+s1 = Solution()
+print(s1.findPeakElement([1, 2]))
