6373ea4b8c1636d39ebcbd9265ff8f6f57c8edee
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-14 15:26:26 +0330

    Adding 136 using python

---
+++ Python/136.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = nums[0]
+        for i in range(1, len(nums)):
+            res ^= nums[i]
+        return res
