ca5f29079a98a3a86b2add2e5f2de1b8b780bccb
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-16 01:23:38 +0330

    Adding 746 using python

---
+++ Python/746.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        if len(cost) < 2:
+            return cost[0]
+        cost.append(0)
+
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
