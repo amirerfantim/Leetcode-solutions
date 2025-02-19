bde9448532684ab9b2729a75c7775b19ae098adc
Amirerfan Teimoori <tamirerfan@gmail.com>
2025-02-19 15:29:06 +0330

    Adding 605 using python

--- Python/605.py
+++ Python/605.py
@@ -3,37 +3,21 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if len(flowerbed) == 0:
-            return False
-        if len(flowerbed) == 1 and flowerbed[0] == 0:
-            n -= 1
-        if len(flowerbed) > 1 and flowerbed[0] == 0 and flowerbed[1] == 0:
-            n -= 1
-            flowerbed[0] = 1
-        if len(flowerbed) > 1 and flowerbed[len(flowerbed) - 1] == 0 and flowerbed[len(flowerbed) -2 ] == 0:
-            n -= 1
-            flowerbed[len(flowerbed) - 1] = 1
-        for i in range(1, len(flowerbed) - 1):
-            if i == 0 and i + 1 < len(flowerbed):
-                if flowerbed[i] == 0 and flowerbed[i + 1] == 0:
-                    n -= 1
-                    flowerbed[i] = 1
-            elif i == len(flowerbed) - 1 and i - 1 >= 0:
-                if flowerbed[i] == 0 and flowerbed[i - 1] == 0:
-                    n -= 1
-                    flowerbed[i] = 1
-            else:
-                if flowerbed[i] == 1:
-                    continue
-                if flowerbed[i] == 0 and (flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0):
-                    n -= 1
-                    flowerbed[i] = 1
-        print(flowerbed)
-        if n <= 0:
-            return True
-        else:
-            return False
+        for i in range(len(flowerbed)):
+            l, r = False, False
+            if flowerbed[i]:
+                continue
+            if i - 1 < 0 or not flowerbed[i - 1]:
+                l = True
+            if i + 1 >= len(flowerbed) or not flowerbed[i + 1]:
+                r = True
+            if r and l:
+                n -= 1
+                flowerbed[i] = 1
+            if n <= 0:
+                return True
+        return not bool(n)
 
 
 s1 = Solution()
-s1.canPlaceFlowers([0, 0, 1, 0, 1], 1)
+print(s1.canPlaceFlowers([1], 0))
